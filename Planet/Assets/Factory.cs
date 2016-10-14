9f9fab5d2e8b95c5901dcec32c45dcb08d01ada6
haleyfuller <haleyfuller@me.com>
2016-10-13 19:15:38 -0700

    Planet files committed

---
+++ Planet/Assets/Factory.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Factory : MonoBehaviour
+{
+	public Observer hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+
+
+	void Start()
+	{
+		
+		StartCoroutine( SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		int rand;
+		while(true)
+		{
+			for (int i = 0; i < hazardCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, Random.Range (-spawnValues.z, spawnValues.z));
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				rand = Random.Range (1, 4);
+
+				if( rand == 1 )
+					hazard.myBehav = new JitterBehavior ();
+				else if( rand == 2 )
+					hazard.myBehav = new StraightBehavior ();
+				else if( rand == 3 )
+					hazard.myBehav = new AngleBehavior ();
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+		}
+	}
+		
+}
---
+++ Planet/Assets/Scripts/RegisterObjects.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RegisterObjects : MonoBehaviour 
+{
+	List < Collider > observers; 
+
+	public RegisterObjects()
+	{
+		observers = new List <Collider>();
+	}
+
+	void OnTriggerEnter( Collider other )
+	{
+		observers.Add (other);
+
+	}
+		
+	void OnTriggerExit( Collider other )
+	{
+		observers.Remove (other);
+	}
+
+	void notifyObservers()
+	{
+		for (int i = 0; i < observers.Count; i++) 
+		{
+			Collider o = observers [i]; 
+
+			Vector3 diff = o.transform.position - transform.position;
+
+			diff.Normalize();
+
+			o.transform.position -= diff * .005f;
+		}
+	}
+
+	void Update()
+	{
+		for (int i = 0; i < observers.Count; i++) 
+		{
+			if (observers [i] == null)
+				observers.Remove (observers [i]);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		notifyObservers ();
+	}
+
+}
+
